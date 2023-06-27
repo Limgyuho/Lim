7c364a43ddbae5eb29614eec6a9ecda8e15b7760
Limgyuho <radum0719@hanmail.net>
2023-06-27 12:40:20 +0900

    파일 전송완료

--- src/main/java/com/koreaIT/demo/chatingController/SocketHandler.java
+++ src/main/java/com/koreaIT/demo/chatingController/SocketHandler.java
@@ -1,29 +1,36 @@
 package com.koreaIT.demo.chatingController;
+
+import java.io.IOException;
+import java.util.Base64;
 import java.util.HashMap;
+
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.BinaryMessage;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+
 @Component
 public class SocketHandler extends TextWebSocketHandler {
     // 웹소켓 세션을 담아둘 맵
-    HashMap<String, ChatSession> sessionMap = new HashMap<>();
+    private HashMap<String, ChatSession> sessionMap = new HashMap<>();
+
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         // 메시지 발송
         String msg = message.getPayload();
         JSONObject obj = jsonToObjectParser(msg);
-       
+
         // 모든 채팅 세션에 메시지 전송
         for (ChatSession chatSession : sessionMap.values()) {
             chatSession.sendMessage(obj.toJSONString());
         }
     }
-    @SuppressWarnings("unchecked")
+
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         // 소켓 연결
@@ -38,14 +45,40 @@ public class SocketHandler extends TextWebSocketHandler {
             JSONObject obj = new JSONObject();
             obj.put("type", "previousMessages");
             obj.put("messages", previousMessages);
-            obj.put("messages", previousMessages);	
             chatSession.sendMessage(obj.toJSONString());
         }
     }
+
+    @Override
+    public void handleBinaryMessage(WebSocketSession session, BinaryMessage message) {
+        // 파일 처리
+        byte[] fileData = message.getPayload().array();
+        String fileName = session.getAttributes().get("fileName").toString();
+        String senderName = session.getAttributes().get("senderName").toString();
+        String roomId = session.getAttributes().get("roomId").toString();
+
+        // 파일 데이터를 Base64로 인코딩
+        String fileDataEncoded = Base64.getEncoder().encodeToString(fileData);
+
+        // 파일 전송 메시지 생성
+        JSONObject obj = new JSONObject();
+        obj.put("type", "file");
+        obj.put("senderName", senderName);
+        obj.put("content", fileDataEncoded);
+        obj.put("roomId", roomId);
+        obj.put("fileName", fileName);
+
+        // 모든 채팅 세션에 파일 전송 메시지 전송
+        for (ChatSession chatSession : sessionMap.values()) {
+            chatSession.sendMessage(obj.toJSONString());
+        }
+    }
+
+
+
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         // 소켓 종료
-      
         // 맵에서 해당 세션 제거 및 ChatSession 닫기
         ChatSession chatSession = sessionMap.remove(session.getId());
         if (chatSession != null) {
@@ -53,6 +86,7 @@ public class SocketHandler extends TextWebSocketHandler {
         }
         super.afterConnectionClosed(session, status);
     }
+
     private static JSONObject jsonToObjectParser(String jsonStr) {
         JSONParser parser = new JSONParser();
         JSONObject obj = null;
@@ -63,17 +97,16 @@ public class SocketHandler extends TextWebSocketHandler {
         }
         return obj;
     }
-//    private static String getSessionAttribute(WebSocketSession session, String attributeName) {
-//        Object attributeValue = session.getAttributes().get(attributeName);
-//        return attributeValue != null ? attributeValue.toString() : null;
-//    }
+
     private class ChatSession {
         private WebSocketSession session;
         private StringBuilder chatHistory;
+
         public ChatSession(WebSocketSession session) {
             this.session = session;
             this.chatHistory = new StringBuilder();
         }
+
         public void sendMessage(String message) {
             try {
                 session.sendMessage(new TextMessage(message));
@@ -81,15 +114,18 @@ public class SocketHandler extends TextWebSocketHandler {
                 e.printStackTrace();
             }
         }
+
         public synchronized void appendToChatHistory(String message) {
             // 대화 내용을 채팅 기록에 추가
             chatHistory.append(message);
             chatHistory.append("\n");
         }
+
         public String getChatHistory() {
             // 채팅 기록 반환
             return chatHistory.toString();
         }
+
         public void close() {
             // 채팅 세션 종료
             session = null;
--- src/main/webapp/WEB-INF/jsp/usr/chat/chat.jsp
+++ src/main/webapp/WEB-INF/jsp/usr/chat/chat.jsp
@@ -15,12 +15,12 @@
         // WebSocket 연결
         var id = "${id}";
         socket = new WebSocket("ws://localhost:8085/chat/" + id);
-        
+
         // 연결이 열린 경우
         socket.onopen = function (event) {
             console.log("WebSocket 연결이 열렸습니다.");
         };
-        
+
         // 메시지 수신
         socket.onmessage = function (event) {
             var message = JSON.parse(event.data);
@@ -28,19 +28,19 @@
             console.log('message', message);
             handleMessage(message);
         };
-        
+
         var currentUserName = "${userName}"; // 로그인한 사용자의 이름
-        
+
         // 연결이 닫힌 경우
         socket.onclose = function (event) {
             console.log("WebSocket 연결이 닫혔습니다.");
         };
-        
+
         // 메시지 전송
         $("#send-button").on("click", function () {
             var messageInput = $("#message-input");
             var message = messageInput.val();
-           
+
             var id = "${id}"; // 방 식별자 값 설정
             if (message.trim() !== "") {
                 var data = {
@@ -52,7 +52,7 @@
                 messageInput.val(""); // 입력 필드 초기화
             }
         });
-        
+
         // 엔터 키 입력 시 메시지 전송
         $("#message-input").on("keydown", function (event) {
             if (event.keyCode === 13) { // 엔터 키
@@ -60,16 +60,22 @@
                 $("#send-button").click();
             }
         });
-        
+
         // 파일 선택
         $("#file-input").on("change", function (event) {
             selectedFile = event.target.files[0];
-            $("#selected-file-info").text("선택한 파일: " + selectedFile.name);
-            $("#download-link").attr("href", "#");
-            $("#download-link").text("다운로드");
-            $("#download-link").hide();
+            if (selectedFile) {
+                $("#selected-file-info").text("선택한 파일: " + selectedFile.name);
+                $("#download-link").attr("href", "#");
+                $("#download-link").text("다운로드");
+                $("#download-link").hide();
+            } else {
+                $("#selected-file-info").text("");
+                $("#download-link").attr("href", "#");
+                $("#download-link").hide();
+            }
         });
-        
+
         // 파일 전송
         $("#send-file-button").on("click", function () {
             if (selectedFile) {
@@ -85,31 +91,26 @@
                     socket.send(JSON.stringify(data));
                 };
                 reader.readAsDataURL(selectedFile);
-
-                selectedFile = null; // 선택한 파일 초기화
-                $("#selected-file-info").text("");
-                $("#download-link").attr("href", "#");
-                $("#download-link").hide();
             }
         });
-        
+
         // 메시지 처리
         function handleMessage(message) {
             console.log(message);
             var senderName = message.senderName;
             var content = message.content;
-            
+
             var chatMessage = '<div class="chat chat-start">\n' +
                 '  <div class="chat-bubble chat-bubble-info">' + senderName + ': ' + content + '</div>\n' +
                 '</div>';
-            
+
             $("#chat-window").append(chatMessage);
             $("#chat-window").scrollTop($("#chat-window")[0].scrollHeight);
-            
+
             // 현재 메시지와 이전 메시지 저장
             var allMessages = $("#chat-window").html();
             localStorage.setItem('chatMessages', allMessages);
-            
+
             // 파일 다운로드 링크 처리
             if (message.fileName) {
                 var fileDownloadLink = '<a href="' + message.content + '" download="' + message.fileName + '">다운로드</a>';
@@ -119,13 +120,13 @@
                 $("#download-link").show();
             }
         }
-        
+
         // 메시지 삭제
         $("#clear-button").on("click", function () {
             $("#chat-window").empty();
             localStorage.removeItem('chatMessages');	
         });
-        
+
         // 뒤로 가기
         $("#back-button").on("click", function () {
             history.back(); // 뒤로 가기 기능 실행
